32ac929061e21ffe3f839a3ffd8568082cd20e5a
Michael Brown and Jared Reando <jared@>
2018-11-14 14:41:11 -0800

    Add counting to basically forever.

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -1,18 +1,22 @@
 require('pry')
 
 def numbers_less_than_100(reverse_number, numbers)
-if(numbers.keys.include?(reverse_number.reverse.to_i))
+  if(numbers.keys.include?(reverse_number.reverse.to_i))
     return (numbers.fetch(reverse_number.reverse.to_i))
 
-  else
-  ones_place = (reverse_number[0]).to_i
-  tens_place = numbers.fetch((reverse_number[1]+'0').to_i) + ' '
-  return (tens_place + numbers.fetch(ones_place))
+    else
+    if reverse_number[0] != 0
+      ones_place = (reverse_number[0]).to_i
+    end
+    if reverse_number[1] != 0
+    tens_place = numbers.fetch((reverse_number[1]+'0').to_i) + ' '
+    end
+    return (tens_place + numbers.fetch(ones_place))
   end
 end
 
-def number_to_word(num)
-  numbers = Hash.new()
+def numbers_less_than_1000(num, number_words_array)
+numbers = Hash.new()
   numbers.store(0,"")
   numbers.store(00,"")
   numbers.store(1,"one")
@@ -43,26 +47,66 @@ def number_to_word(num)
   numbers.store(18,"eighteen")
   numbers.store(19,"nineteen")
 
-  number_words_array = []
-  reverse_number = num.to_s.reverse
-
   if(numbers.keys.include?(num))
     number_words_array.push(numbers.fetch(num))
-
   else
-  if (numbers.keys.include?(reverse_number[2].to_i))
-      hundreds_place = (numbers.fetch(reverse_number[2].to_i))+' hundred'
+  if (numbers.keys.include?(num[2].to_i))
+      hundreds_place = (numbers.fetch(num[2].to_i))+' hundred'
       if hundreds_place != ' hundred'
       number_words_array.push(hundreds_place)
       end
     end
-    hundreds_value = reverse_number.slice(2,1).to_i
-    tens_and_ones_value = reverse_number.slice(0,2)
+    hundreds_value = num.slice(2,1).to_i
+    tens_and_ones_value = num.slice(0,2)
 
-    number_words_array.push(numbers_less_than_100(tens_and_ones_value, numbers))
+    lower_digits = numbers_less_than_100(tens_and_ones_value, numbers)
 
+    if lower_digits != ''
+      number_words_array.push(lower_digits)
+    end
+  end
+end
 
+def number_to_word(input_num)
 
+  literally_anything_we_want = Hash.new()
+  literally_anything_we_want.store(1, '')
+  literally_anything_we_want.store(2, 'thousand')
+  literally_anything_we_want.store(3, 'million')
+  literally_anything_we_want.store(4, 'billion')
+  literally_anything_we_want.store(5, 'trillion')
+  literally_anything_we_want.store(6, 'quadrillion')
+  literally_anything_we_want.store(7, 'quintillion')
+  literally_anything_we_want.store(8, 'sextillion')
+  literally_anything_we_want.store(9, 'septillion')
+  literally_anything_we_want.store(10, 'octillion')
+  literally_anything_we_want.store(11, 'nonillion')
+  literally_anything_we_want.store(12, 'decillion')
+  literally_anything_we_want.store(13, 'undecillion')
+  literally_anything_we_want.store(14, 'duodecillion')
+  literally_anything_we_want.store(15, 'tredecillion')
+  literally_anything_we_want.store(16, 'quattuordecillion')
+  literally_anything_we_want.store(17, 'quindecillion')
+  literally_anything_we_want.store(18, 'sexdecillion')
+  literally_anything_we_want.store(19, 'septdecillion')
+  literally_anything_we_want.store(20, 'octodecillion')
+  literally_anything_we_want.store(21, 'novemdecillion')
+  literally_anything_we_want.store(22, 'vigintillion')
+
+  number_words_array = []
+
+  reverse_number = input_num.to_s.reverse.split('')
+  super_array = reverse_number.each_slice(3).to_a.reverse
+
+  thousands_position = super_array.length
+
+  super_array.each do |array|
+    num = array.join
+    numbers_less_than_1000(num, number_words_array)
+    if thousands_position > 1 && num != '000'
+      number_words_array.push(literally_anything_we_want.fetch(thousands_position))
+    end
+    thousands_position -= 1
   end
   number_words_array.join(' ')
 end
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -17,4 +17,22 @@ describe('number_to_word') do
   it('Should return any number from 1 to 999') do
     expect(number_to_word(184)).to(eq("one hundred eighty four"))
   end
+
+  it('Should return a number from 1 to 9,999') do
+    expect(number_to_word(1001)).to(eq("one thousand one"))
+  end
+
+  it("Should return a number from 1 to 1,000,000,000,000") do
+    expect(number_to_word(1000000000000)).to(eq("one trillion"))
+  end
+
+  it("Should return a number from 1 to 40,089,789,009,799") do
+    expect(number_to_word(40089789009799)).to(eq("fourty trillion eighty nine billion seven hundred eighty nine million nine thousand seven hundred ninety nine"))
+  end
+
+  it("Should return any number you can reasonably count to in this iteration of human evolution.") do
+    expect(number_to_word(400897890978982798798798797030978534098340597340957397489876543213)).to(eq("four hundred vigintillion eight hundred ninety seven novemdecillion eight hundred ninety octodecillion nine hundred seventy eight septdecillion nine hundred eighty two sexdecillion seven hundred ninety eight quindecillion seven hundred ninety eight quattuordecillion seven hundred ninety eight tredecillion seven hundred ninety seven duodecillion thirty undecillion nine hundred seventy eight decillion five hundred thirty four nonillion ninety eight octillion three hundred fourty septillion five hundred ninety seven sextillion three hundred fourty quintillion nine hundred fifty seven quadrillion three hundred ninety seven trillion four hundred eighty nine billion eight hundred seventy six million five hundred fourty three thousand two hundred thirteen"))
+  end
+
+
 end
